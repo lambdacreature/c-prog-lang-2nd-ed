907af9b3335d80de986b2f4eb63d28090837bdab
sidhe <lambdacreature@gmail.com>
2026-02-01 10:59:28 -0500

    ejercicio 1.3

---
+++ ch01/exercises/ex-1-3-center-fahr-cel-heading.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/*
+ * Muestra un encabezamiento antes de la tabla
+ * de Fahrenheit a Celsius, far = 0.0 hasta 300.0
+ *
+ */
+
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0.0;
+	upper = 300.0;
+	step  = 20.0;
+
+	printf("Fahrenheit Celsius\n");
+	/*      \        / \     /
+	 *       \      /   \   /
+	 *        \    /     \ /
+	 *         \  /       7
+	 *          \/
+	 *          10
+	 */ 
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%6.0f      %5.1f\n", fahr, celsius);
+
+		fahr = fahr + step;
+	}
+}
